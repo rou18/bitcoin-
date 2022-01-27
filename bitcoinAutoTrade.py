d0753aed0b7448523c647e0551de41a04d841c66
rou18 <91887494+rou18@users.noreply.github.com>
2022-01-27 11:34:33 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -42,8 +42,8 @@ def get_trade_time(ticker):                         # 최근 거래 채결 날�
 ##########################################################################################################
 
 # 로그인
-access = "MW2yVTosalLYP1H2AtxR7jlULslQm8ZwdxGN57UM"
-secret = "zD1YInLsghc8dBgTNyvpVYdgf5HPP1itinVyYc0E"
+access = ""
+secret = ""
 
 upbit = pyupbit.Upbit(access, secret)
 print("Login OK")
